2a3344123f4554ab8fe7156d3ac718c99b0339c0
ditanh97 <51126784+ditanh97@users.noreply.github.com>
2019-09-21 13:38:48 +0700

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,39 @@
+function biodataDiri(){
+  return{
+    "name": "ditut",
+    "age": 22,
+    "address": "Jalan menuju-Nya",
+    "hobbies": [
+      "learning",
+      "pondering",
+      "eating",
+      "watching"
+    ],
+    "is_married": false,
+    "list_school": [
+      {
+        "name": "SMANDA",
+        "year_in": 2012,
+        "year_out": 2014,
+        "major": "Science"
+      },
+      {
+        "name": "BINSUS",
+        "year_in": 2009,
+        "year_out": 2012,
+        "major": null
+      }
+    ],
+    "skills": [
+      {
+        "skill_name": "watching",
+        "level": "expert"
+      },
+      {
+        "skill_name": "eating",
+        "level": "beginner"
+      }
+    ],
+    "interest_in_coding": true
+  }
+}
